55e9758d4e70130d142ee94d3b6e657f22302104
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2021-11-11 19:09:15 +0400

    Make faq responsive

--- src/modules/HomePage/SeventhSection.vue
+++ src/modules/HomePage/SeventhSection.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main-page-seventh-section">
-    <p class="faq-title">ხშირად დასმული შეკითხვები</p>
+    <p class="faq-title">ხშირად დასმული კითხვები</p>
     <p class="contact-title">დაგვიკავშირდი</p>
     <ul class="faq-list">
       <li class="" v-for="(item, index) in faqData" :key="index">
@@ -33,7 +33,7 @@ export default {
         },
         {
           uuid: '2',
-          question: 'როგორ ავირიდო არასასურველი მომწოდებელი?',
+          question: 'როგორ ავირიდო არასასურველი მომწოდებელი?როგორ ავირიდო არასასურველი მომწოდებელი?როგორ ავირიდო არასასურველი მომწოდებელი?',
           answer: 'მომხმარებელს აქვს შესაძლებლობა კლასტერში გაწევრიანებისას განაცხადოს რომ ნებისმიერი შედეგის შემთხვევაში, არ მიიღებს პროცესში მონაწილე რომელიმე მიმწოდებლის პროდუქტს. მთავარია, ამის შესახებ კლასტერის მენეჯერს ეცნობოს კომპანიის რეგისტრაციისას',
           isActive: false,
           style: {display: 'none'}
@@ -75,6 +75,12 @@ export default {
   background: linear-gradient(162.38deg, #8B63FF -57.19%, #6335E9 87.95%);
 }
 
+@media all and (max-width: 480px) {
+  .main-page-seventh-section {
+    height: 1374px;
+  }
+}
+
 .faq-title {
   position: absolute;
   width: 699px;
@@ -86,6 +92,16 @@ export default {
   line-height: 37px;
   color: #FFFFFF;
   margin: 120px 0 0 114px;
+}
+
+@media all and (max-width: 480px) {
+  .faq-title {
+    width: 372px;
+    height: 52px;
+    font-size: 24px;
+    line-height: 170%;
+    margin: 100px 0 0 35px;
+  }
 }
 
 .contact-title {
@@ -118,7 +134,7 @@ export default {
 .faq-list-item {
   position: relative;
   width: 583px;
-  height: 67px;
+  height: auto;
   border: 1px solid #DEE2E6;
   box-sizing: border-box;
   border-radius: 4px;
@@ -130,9 +146,10 @@ export default {
   font-style: normal;
   font-weight: bold;
   font-size: 18px;
-  line-height: 21px;
+  line-height: 180%;
   color: #FFFFFF;
-  margin: 22px 0 0 24px;
+  min-height: 40px;
+  margin: 22px 0 10px 24px;
 }
 
 .toggle-icon {
@@ -142,14 +159,38 @@ export default {
 }
 
 .active-question-description {
-  height: 100px;
-  width: 545px;
+  height: auto;
+  width: auto;
+  max-width: 545px;
   font-family: Helvetica, sans-serif;
   font-style: normal;
   font-weight: normal;
   font-size: 16px;
   line-height: 170%;
   color: #FFFFFF;
-  margin: 32px 0 40px 29px;
+  margin: 32px 40px 40px 29px;
+}
+
+@media all and (max-width: 480px) {
+  .faq-list {
+    margin-top: 195px;
+    margin-left: 5%;
+
+    > li {
+      margin-right: unset;
+    }
+  }
+
+  .faq-list-item {
+    width: 334px;
+  }
+
+  .faq-list-question {
+    font-size: 16px;
+  }
+
+  .active-question-description {
+    font-size: 16px;
+  }
 }
 </style>
